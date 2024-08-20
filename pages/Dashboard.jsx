528e000920633dfacb41553ea10a743bebf71f3f
John Z <john.zissimou@and.digital>
2024-08-20 11:03:31 +0100

    Create space card component and added it to Dashboard.

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { StyleSheet, Pressable } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Space from '../components/Space';
 
 const Dashboard = () => {
     return (
         <SafeAreaView style={styles.container}>
+            <Space spaceName={"House of the Dragons"} />
+            <Space spaceName={"Fast & Furious"} />
             <Pressable style={styles.addNewButton}>
                 <Ionicons name="add-circle-sharp" size={70} color="#CDA7AF" />
             </Pressable>
@@ -17,8 +20,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#E2CFC8',
-        alignItems: 'center',
-        justifyContent: 'center'
+        alignItems: 'center'
     },
     addNewButton: {
         position: 'absolute',
